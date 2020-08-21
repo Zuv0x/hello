516355f96f358daca32bb6c309caff38980dbd8a
unknown <odiseo.rey@gmail.com>
2020-08-21 12:23:14 -0500

    Datos tipo entero y condificonal

---
+++ name.py
@@ -0,0 +1,11 @@
+# Solicitando datos
+name=input("Name: ")
+print("Hello "+name)
+#formato de String
+print(f"Hello {name}, Bienvenido.")
+#Número de interger
+n=input("Ingrese Número: ")
+if n > 0:
+    print("n is positive")
+else:
+    print("n is not positive")
